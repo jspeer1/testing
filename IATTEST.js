a46c93359bfd3d62c8c575b76c345953e762a7d1
jspeer1 <103079131+jspeer1@users.noreply.github.com>
2022-04-07 14:15:55 -0700

    Update IATTEST.js

--- IATTEST.js
+++ IATTEST.js
@@ -1,13 +1,4 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2580
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fmodern\fcharset0 Courier;}
-{\colortbl;\red255\green255\blue255;\red0\green0\blue0;}
-{\*\expandedcolortbl;;\cssrgb\c0\c0\c0;}
-\margl1440\margr1440\vieww20700\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\partightenfactor0
-
-\f0\fs24 \cf2 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec2 define(['pipAPI',  'https://raw.githubusercontent.com/jspeer1/testing/main/IATTEST\'92], function(APIConstructor, iatExtension)\{\
+define(['pipAPI',  'https://cdn.jsdelivr.net/gh/jspeer1/testing/IATTEST.js'], function(APIConstructor, iatExtension)\{\
 	\
 	var API = new APIConstructor();\
 	var set = API.shuffle(['A','B'])[0];\
